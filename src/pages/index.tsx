921d2a8e694b248a66c3fddb38762aa2a81205d3
FabioJunik <fabiojunik@gmail.com>
2022-06-18 21:20:41 +0100

    Página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import Head  from "../../node_modules/next/head";
+import  styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | Ig.news</title>
+        <title>Home | Ig.news</title>
       </Head>
-      <h1>Hello Word</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>News about <br /> the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
